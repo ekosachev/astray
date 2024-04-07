297a7ff466a34c79fbe8efb570688e7bc17ce62e
Vinermy <egorkosachev@vk.com>
2024-04-07 18:26:56 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Astray
+
+[![CI](https://github.com//astray/workflows/CI/badge.svg)](https://github.com//astray/actions)
+
+A TUI-based replica of Aurora 4X
---
+++ src/cli.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version = env!("CARGO_PKG_VERSION"), about)]
+pub struct Cli {
+  #[arg(short, long, value_name = "FLOAT", help = "Tick rate, i.e. number of ticks per second", default_value_t = 1.0)]
+  pub tick_rate: f64,
+
+  #[arg(
+    short,
+    long,
+    value_name = "FLOAT",
+    help = "Frame rate, i.e. number of frames per second",
+    default_value_t = 4.0
+  )]
+  pub frame_rate: f64,
+}
