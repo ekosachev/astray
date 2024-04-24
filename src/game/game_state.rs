62a2bc85e6b109bbd7544ba909352bfb4275fda5
Vinermy <egorkosachev@vk.com>
2024-04-24 23:25:59 +0700

    Added research fields

--- src/game/game_state.rs
+++ src/game/game_state.rs
@@ -1,10 +1,11 @@
 use crate::game::celestial_bodies::CelestialBody;
 use crate::game::celestial_bodies::solar_system::SolarSystem;
-use crate::game::research::Research;
+use crate::game::research::{Research, ResearchField};
 
 pub struct GameState {
     systems: Vec<SolarSystem>,
     research: Vec<Research>,
+    research_fields: Vec<ResearchField>,
 }
 
 impl Default for GameState {
@@ -12,6 +13,7 @@ impl Default for GameState {
         Self {
             systems: vec![SolarSystem::generate(())],
             research: Research::load_from_file("assets/research.json5"),
+            research_fields: ResearchField::load_from_file("assets/research_fields.json5"),
         }
     }
 }
@@ -24,4 +26,7 @@ impl GameState {
     pub fn get_starting_system(&self) -> SolarSystem {
         self.systems[0].clone()
     }
+    
+    pub fn get_research_fields(&self) -> Vec<ResearchField> { self.research_fields.clone() }
+    pub fn get_researches(&self) -> Vec<Research> { self.research.clone() }
 }
--- src/game/research.rs
+++ src/game/research.rs
@@ -1,18 +1,30 @@
+use std::char::decode_utf16;
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
+pub struct ResearchField {
+    name: String,
+    id: String,
+}
+
+#[derive(Deserialize, Clone)]
 pub struct Research {
     name: String,
     id: String,
     cost: u32,
+    field: String,
     required_any: Vec<String>,
     required_all: Vec<String>,
 }
 
+impl ResearchField {
+    pub fn load_from_file(filepath: &str) -> Vec<Self> {
+        serde_json::from_str(&std::fs::read_to_string(filepath).unwrap()).unwrap()
+    }
+}
+
 impl Research {
     pub fn load_from_file(filepath: &str) -> Vec<Self> {
-        let deserialized: Vec<Self> = serde_json::from_str(&std::fs::read_to_string(filepath).unwrap()).unwrap();
-
-        deserialized
+        serde_json::from_str(&std::fs::read_to_string(filepath).unwrap()).unwrap()
     }
 }
